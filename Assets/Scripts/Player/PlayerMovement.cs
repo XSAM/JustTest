3cec1c850bd64a6c5156ab94ffa3e06025fc636a
XSAM <XSAM@github.com>
2016-02-04 12:24:04 +0800

    Set up Plane & Player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public float moveSpeed =10f;
+    public float turnSpeed = 180f;
+    public float jumpSpeed = 5f;
+
+
+    private new Rigidbody rigidbody;
+    private float movementValue;
+    private float turnValue;
+
+    private Vector3 movement;
+    private Quaternion turn;
+    // Use this for initialization
+    void Start () 
+	{
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        movementValue = Input.GetAxis("Vertical");
+        turnValue = Input.GetAxis("Horizontal");
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+        Move();
+        Turn();
+        Jump();
+    }
+    
+    void Move()
+    {
+        movement = transform.forward * movementValue * moveSpeed * Time.deltaTime;
+        rigidbody.MovePosition(rigidbody.position + movement);
+    }
+    
+    void Turn()
+    {
+        turn = Quaternion.Euler(new Vector3(0f, turnValue, 0f) * turnSpeed * Time.deltaTime);
+        rigidbody.MoveRotation(rigidbody.rotation * turn);
+    }
+    
+    void Jump()
+    {
+        if(Input.GetButton("Jump"))
+        {
+            rigidbody.velocity = transform.up * jumpSpeed;
+        }
+    }
+}
