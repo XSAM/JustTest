daf9aa54755c4938baa26dffbb73809b623b505a
XSAM <XSAM@github.com>
2016-02-04 23:17:50 +0800

    Update

--- Assets/Scripts/Player/Player_SyncPosition.cs
+++ Assets/Scripts/Player/Player_SyncPosition.cs
@@ -34,7 +34,7 @@ public class Player_SyncPosition : NetworkBehaviour
         syncRotate = rotate;
     }
     
-    [ClientCallbackAttribute]
+    [ClientAttribute]
     void TransmitPosition()
     {
         if(isLocalPlayer)
