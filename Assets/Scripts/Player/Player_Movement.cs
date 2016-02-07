7af13ffa94ad097c9e8a421ca21283941b260167
XSAM <XSAM@github.com>
2016-02-07 19:50:44 +0800

    Emulate 50ms network delay conditions

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player_Movement : MonoBehaviour 
+public class Player_Movement : MonoBehaviour
 {
-    public float moveSpeed =10f;
+    public float moveSpeed = 10f;
     public float turnSpeed = 180f;
     public float jumpSpeed = 5f;
 
@@ -15,44 +15,62 @@ public class Player_Movement : MonoBehaviour
 
     //private Vector3 movement;
     private Quaternion turn;
+    private bool isFirstMove = true;
+    private float firstTime = 100f;
     // Use this for initialization
-    void Start () 
-	{
+    void Start()
+    {
         rigidbody = GetComponent<Rigidbody>();
-        transform.position=new Vector3(5f, 1f, 0f);
+        transform.position = new Vector3(5f, 1f, 0f);
         transform.Rotate(new Vector3(0f, 160f, 0f));
     }
-    
+
     void Update()
     {
         movementValue = Input.GetAxis("Vertical");
         turnValue = Input.GetAxis("Horizontal");
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         Move();
         Turn();
         Jump();
     }
-    
+
     void Move()
     {
+        Vector3 zeroTemp = new Vector3(0f, 0f, 0f);
         Vector3 movement = transform.forward * movementValue * moveSpeed * Time.deltaTime;
-        rigidbody.MovePosition(rigidbody.position + movement);
+        //模拟50ms网络延迟不加优化的行走状态
+        if (isFirstMove && movement != zeroTemp)
+        {
+            firstTime = Time.realtimeSinceStartup;
+            isFirstMove = false;
+        }
+        if (firstTime + 0.1f <= Time.realtimeSinceStartup)
+        {
+            //Debug.Log("DelayTime:" + (Time.realtimeSinceStartup - firstTime));
+            //Debug.Log("FirstTime:"+firstTime);
+            rigidbody.MovePosition(rigidbody.position + movement);
+            isFirstMove = true;
+            firstTime = 2000f;
+            
+        }
+
     }
-    
+
     void Turn()
     {
         turn = Quaternion.Euler(new Vector3(0f, turnValue, 0f) * turnSpeed * Time.deltaTime);
-        
+
         rigidbody.MoveRotation(rigidbody.rotation * turn);
     }
-    
+
     void Jump()
     {
-        if(Input.GetButton("Jump"))
+        if (Input.GetButton("Jump"))
         {
             rigidbody.velocity = transform.up * jumpSpeed;
         }
